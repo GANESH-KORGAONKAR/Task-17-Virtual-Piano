cc40a74939d9d46e5934c9f2c7bfa1dc5dd581ad
GANESH-KORGAONKAR <ganeshkorgaonkar4335@gmail.com>
2026-02-17 15:46:47 +0530

    added event listners and audio

---
+++ script.js
@@ -0,0 +1,56 @@
+const keys = document.querySelectorAll(".Key");
+
+// Preload all sounds
+const sounds = {};
+
+keys.forEach(key => {
+  let keyNumber = key.dataset.key;
+
+  // Add leading zero if number < 10
+  if (keyNumber < 10) {
+    keyNumber = "0" + keyNumber;
+  }
+
+  sounds[key.dataset.key] = new Audio(`24-piano-keys/key${keyNumber}.mp3`);
+});
+
+// Play sound
+function playSound(number) {
+  const audio = sounds[number];
+  audio.currentTime = 0;
+  audio.play();
+}
+
+// Activate visual effect
+function activateKey(key) {
+  key.classList.add("active");
+
+  setTimeout(() => {
+    key.classList.remove("active");
+  }, 100);
+}
+
+// Mouse click
+keys.forEach(key => {
+  key.addEventListener("click", () => {
+    const number = key.dataset.key;
+    playSound(number);
+    activateKey(key);
+  });
+});
+
+// Keyboard press
+document.addEventListener("keydown", (e) => {
+  const pressedKey = e.key.toUpperCase();
+
+  keys.forEach(key => {
+    if (key.textContent === pressedKey) {
+      const number = key.dataset.key;
+      playSound(number);
+      activateKey(key);
+    }
+  });
+
+  console.log("Pressed:", e.key);
+});
+
